af97f99431c3048c4a0e8e924dcf0b448aa2a2a7
Amir TOLY <amir.toly@gmail.com>
2014-10-14 09:41:32 +0200

    Complete Restricted Access Page exercise (1 file instead of 2)

---
+++ ex2/secret.php
@@ -0,0 +1,40 @@
+<?php
+
+$pwd = $_POST['pwd'];
+
+if (!isset($pwd) && htmlspecialchars($pwd) != 'whale')
+{
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Restricted access - PHP Tutorial</title>
+	</head>
+	
+	<body>
+		<form method="POST" action="secret.php">
+			<label for="pwd">Please, enter password:</label>
+			<br/>
+			<input type="password" id="pwd" name="pwd"/>
+			<br/>
+			<input type="submit"/>
+		</form>
+	</body>
+</html>
+
+<?php
+}
+elseif (isset($pwd) && htmlspecialchars($pwd) != 'whale')
+{
+	echo 'Wrong password, try again! <br/>'.
+		'<a href="secret.php">Enter password</a>';
+}
+else
+{
+	echo 'Welcome to this restricted area. <br/>' .
+	'The secret message is: CONGRATULATIONS!';
+}
+
+?>
+
