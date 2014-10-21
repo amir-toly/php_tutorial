ec1dd14c9519868519b01b9493d30b29b1cfbc29
Amir TOLY <amir.toly@gmail.com>
2014-10-21 18:43:40 +0200

    Complete Small Chat exercise

---
+++ ex3/chat.php
@@ -0,0 +1,53 @@
+<?php
+
+try
+{
+	$db = new PDO(
+		'mysql:host=localhost;dbname=test;',
+		'root',
+		'',
+		array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+	);
+}
+catch (Exception $e)
+{
+	die('ERROR: ' . $e->getMessage());
+}
+
+$response = $db->query('SELECT nickname, message FROM chat ORDER BY id DESC');
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf8" />
+		<title>Chat</title>
+	</head>
+	
+	<body>
+		<form action="chat_post.php" method="post">
+			Nickname:
+			<br/>
+			<input type="text" name="nickname" />
+			<br/>
+			Message:
+			<br/>
+			<textarea name="message"/></textarea>
+			<br/>
+			<input type="submit" value="Send" />
+		</form>
+		<?php while ($data = $response->fetch()) { ?>
+			<p>
+				<strong><?php echo $data['nickname']; ?>:</strong>
+				<?php echo $data['message']; ?>
+			</p>
+		<?php } ?>
+	</body>
+</html>
+
+<?php
+
+$response->closeCursor();
+
+?>
+
---
+++ ex3/chat_post.php
@@ -0,0 +1,48 @@
+<?php
+
+try
+{
+	$db = new PDO(
+		'mysql:host=localhost;dbname=test;',
+		'root',
+		'',
+		array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+	);
+}
+catch (Exception $e)
+{
+	die('ERROR: ' . $e->getMessage());
+}
+
+$nickname = $_POST['nickname'];
+$message = $_POST['message'];
+
+if (isset($nickname) && isset($message))
+{
+	$nickname = strip_tags($nickname);
+	$message = strip_tags($message);
+	
+	if (strlen($nickname) > 0 && strlen($message) > 0)
+	{
+		$query = $db->prepare(
+			'INSERT INTO chat(nickname, message) VALUES(:nickname, :message)'
+		);
+		
+		$query->execute(array(
+			'nickname' => $nickname,
+			'message' => $message
+		));
+	}
+	else
+	{
+		echo '<p>You must provide a nickname AND a message!</p>';
+		echo '<a href="chat.php">Back</a>';
+		
+		return; // Avoid header() call
+	}
+}
+
+header('Location: chat.php');
+
+?>
+
