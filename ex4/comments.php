2b94d73881f671282d4bdb7c19688cac7e567588
Amir TOLY <amir.toly@gmail.com>
2014-10-24 19:21:02 +0200

    Complete A Blog With Comments exercise

---
+++ ex4/comments.php
@@ -0,0 +1,99 @@
+<?php
+
+try
+{
+	$db = new PDO(
+		'mysql:host=localhost;dbname=test',
+		'root',
+		'',
+		array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+	);
+}
+catch (Exception $e)
+{
+	die('ERROR: ' . $e->getMessage());
+}
+
+$post_id = $_GET['post_id'];
+
+if (isset($post_id))
+{
+	$post_id = intval($post_id);
+	
+	$query = $db->prepare(
+		"SELECT title, content, DATE_FORMAT(created_at, '%d/%m/%Y at %Hh%imin%ss') AS created_at " .
+		"FROM posts " .
+		"WHERE id = :post_id"
+	);
+	$query->execute(array(
+		'post_id' => $post_id
+	));
+	
+	$post = $query->fetch();
+	
+	$query->closeCursor();
+	
+	$query = $db->prepare(
+		"SELECT author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y at %Hh%imin%ss') AS comment_date " .
+		"FROM comments " .
+		"WHERE post_id = :post_id " .
+		"ORDER BY comment_date"
+	);
+	
+	$query->execute(array(
+		'post_id' => $post_id
+	));
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My awesome blog!</title>
+		<meta charset="utf8" />
+		<link rel="stylesheet" href="style.css" type="text/css" />
+	</head>
+	
+	<body>
+		<h1>My awesome blog!</h1>
+		<a href="index.php">Back to posts list</a>
+		
+		<div class="news">
+			<h3>
+				<?php echo $post['title']; ?>
+				<em> on (french fashion display) <?php echo $post['created_at']; ?></em>
+			</h3>
+			<p><?php echo $post['content']; ?></p>
+		</div>
+		
+		<h2>Comments</h2>
+		<?php
+		
+		if ($query->rowCount() > 0)
+		{
+			while ($data = $query->fetch()) {
+				
+		?>
+				<p>
+					<strong><?php echo $data['author']; ?></strong>
+					on (french fashion display) <?php echo $data['comment_date']; ?>
+				</p>
+				<p><?php echo $data['comment']; ?></p>
+		<?php
+			
+			}
+		} else {
+			echo '<p><em>No comments for this post</em></p>';
+		}
+		
+		?>
+	</body>
+</html>
+
+<?php
+
+$query->closeCursor();
+
+?>
+
---
+++ ex4/index.php
@@ -0,0 +1,58 @@
+<?php
+
+try
+{
+	$db = new PDO(
+		'mysql:host=localhost;dbname=test',
+		'root',
+		'',
+		array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+	);
+}
+catch (Exception $e)
+{
+	die('ERROR: ' . $e->getMessage());
+}
+
+$response = $db->query("" .
+	"SELECT title, id, content, DATE_FORMAT(created_at, '%d/%m/%Y at %Hh%imin%ss') AS created_at " .
+	"FROM posts " .
+	"ORDER BY created_at DESC"
+);
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My awesome blog!</title>
+		<meta charset="utf8" />
+		<link rel="stylesheet" href="style.css" type="text/css" />
+	</head>
+	
+	<body>
+		<h1>My awesome blog!</h1>
+		<p>Last blog posts:</p>
+		
+		<?php while ($data = $response->fetch()) { ?>
+			<div class="news">
+				<h3>
+					<?php echo $data['title']; ?>
+					<em>on (french fashion display) <?php echo $data['created_at']; ?></em>
+				</h3>
+				<p>
+					<?php echo $data['content'];/*TODO(display line breaks)*/ ?>
+					<br/>
+					<a href="comments.php?post_id=<?php echo $data['id']; ?>"><em>Comments</em></a>
+				</p>
+			</div>
+		<?php } ?>
+	</body>
+</html>
+
+<?php
+
+$response->closeCursor();
+
+?>
+
