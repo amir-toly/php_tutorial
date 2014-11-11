570109d2bcf1925d215356e4eb0bce7482f8e125
Amir TOLY <amir.toly@gmail.com>
2014-11-11 20:01:07 +0100

    Add pagination

--- ex4/index.php
+++ ex4/index.php
@@ -2,12 +2,50 @@
 
 include 'db.php';
 
-$response = $db->query("" .
+function display_pagination() {
+	global $page, $nb_pages;
+	
+	echo '<p>Page';
+	
+	for ($i = 1; $i < $nb_pages; $i++)
+	{
+		if ($i !== $page)
+			echo ' <a href="index.php?page=' . $i . '">' . $i . '</a>';
+		else
+			echo ' ' . $i;
+	}
+	
+	echo '</p>';
+}
+
+define('NB_POSTS_PER_PAGE', 5);
+
+$page = (int) $_GET['page'];
+
+if ($page === 0)
+	$page++;
+
+// Get the number of posts
+$response = $db->query('SELECT COUNT(*) AS nb_posts FROM posts');
+$data = $response->fetch();
+$nb_posts = $data['nb_posts'];
+
+$response->closeCursor();
+
+// Get the number of pages
+$remainder = $nb_posts % NB_POSTS_PER_PAGE;
+$nb_pages = $nb_posts / NB_POSTS_PER_PAGE + ($remainder > 0 ? 1 : 0);
+
+$offset = ($page - 1) * NB_POSTS_PER_PAGE;
+
+$query = $db->prepare("" .
 	"SELECT title, id, content, DATE_FORMAT(created_at, '%d/%m/%Y at %Hh%imin%ss') AS created_at_french_fashion " .
 	"FROM posts " .
 	"ORDER BY created_at DESC " .
-	"LIMIT 0, 5"
+	"LIMIT :offset, 5"
 );
+$query->bindParam(':offset', $offset, PDO::PARAM_INT);
+$query->execute();
 
 ?>
 
@@ -21,9 +59,14 @@ $response = $db->query("" .
 	
 	<body>
 		<h1>My awesome blog!</h1>
-		<p>Last blog posts:</p>
+		<?php display_pagination(); ?>
 		
-		<?php while ($data = $response->fetch()) { ?>
+		<?php
+		
+		while ($data = $query->fetch())
+		{
+		
+		?>
 			<div class="news">
 				<h3>
 					<?php echo htmlspecialchars($data['title']); ?>
@@ -35,13 +78,15 @@ $response = $db->query("" .
 					<a href="comments.php?post_id=<?php echo $data['id']; ?>"><em>Comments</em></a>
 				</p>
 			</div>
-		<?php } ?>
+		<?php
+		
+		}
+		
+		$query->closeCursor();
+		
+		display_pagination();
+		
+		?>
 	</body>
 </html>
-
-<?php
-
-$response->closeCursor();
-
-?>
 
